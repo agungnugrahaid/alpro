af995e58d9babef8e69a9e377c4bf47fd8950c65
agungnugrahaid <31094335+agungnugrahaid@users.noreply.github.com>
2024-10-24 09:22:20 +0000

    Deklarasi Prosedur dan Fungsi

--- bankAbc.c
+++ bankAbc.c
@@ -1,6 +1,57 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 
+//konfigurasi tipe data string mempunyai panjang 255 karakter
+typedef char string[255];
+
+//deklarasi prosedur dan fungsi pada program
+void prosedurDaftarNasabah(string *nama, string *alamat, int *ktp, int *pin, int setor[], int tarik[], bool *blokirStatus, int *saldo);
+void prosedurSetorTarik(int arr[]);
+void prosedurDisplayNasabah(string nama, string alamat, int ktp, int saldo);
+void prosedurDisplaySetorTarik(int setor[], int tarik[]);
+bool fungsiCheckPIN(bool *blokirStatus, int pin);
+int fungsiSetorUang(bool *blokirStatus, int pin, int saldo, int setor[]);
+int fungsiTarikUang(bool *blokirStatus, int pin, int saldo, int tarik[]);
+
+
+//program utama
 int main()
 {
-    printf("something\n");
+    printf("something\n");   
+}
+
+void prosedurDaftarNasabah(string *nama, string *alamat, int *ktp, int *pin, int setor[], int tarik[], bool *blokirStatus, int *saldo)
+{
+    
+}
+
+void prosedurSetorTarik(int arr[])
+{
+    
+}
+
+void prosedurDisplayNasabah(string nama, string alamat, int ktp, int saldo)
+{
+
+}
+
+void prosedurDisplaySetorTarik(int setor[], int tarik[])
+{
+
+}
+
+bool fungsiCheckPIN(bool *blokirStatus, int pin)
+{
+    return 0;
+}
+
+int fungsiSetorUang(bool *blokirStatus, int pin, int saldo, int setor[])
+{
+    return 0;
+}
+
+int fungsiTarikUang(bool *blokirStatus, int pin, int saldo, int tarik[])
+{
+    return 0;
 }
